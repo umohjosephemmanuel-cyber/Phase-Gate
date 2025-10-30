1b984060baf37087b9ea141feea718eef1fe68e6
umohjosephemmanuel-cyber <umohjosephemmanuel@gmail.com>
2025-10-30 14:05:54 +0100

    first commit

---
+++ Day 4/Java/Palindrome.java
@@ -0,0 +1,19 @@
+public class Palindrome {
+	public static void main(String[] args) {
+	String letter[] = ["madam", "racecar", "hello", "noon"];
+	System.out.println(PalindromeString.palindromeNumber(number));
+}
+
+public static String palindromeNumber(String number) {
+	String palindrome = "";
+	for(int count = number.length(); count >=0; count --);
+	if(count == evennumber) {
+	System.out.println("True");
+	} else if(count == oddnumber) {
+	System.out.println("False");
+
+	palindrome += number.length(count);
+}
+return True or False;
+}
+}
